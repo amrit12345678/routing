098ecdced2404a855a5767f2ad7f603fc8d2f236
amrit12345678 <np01cp4a180141@islingtoncollege.edu.np>
2021-03-08 15:59:20 +0545

    bootstrap card and background

--- src/login.js
+++ src/login.js
@@ -1,35 +1,47 @@
 import React, { Component } from 'react';
-import './App.css'
+import './App.css';
+import Logo from './images/cauli.jpg';
 
 class Login extends Component {
     render() {
         return (
             <div className='hello'>
-                <br />
-                <h1 className='loginregister'>Login Below</h1>
-                <input
-                    className='field'
-                    type='username'
-                    placeholder='Username'
-                />
-                <br />
-                <input
-                    className='field'
-                    type='password'
-                    placeholder='Password'
-                />
-                <br />
-                <a href='/registration'>
-                    <button className='button btn-secondary'>
-                        Login
-                    </button>
-                 </a>
-                 <footer className='footer'>
+                <div className='images'>
+                    <img className='logo' src={Logo} />
+                </div>
+                <div className='forcard' style={{ paddingLeft: '650px' }}>
+                    <div className="card" style={{ width: '25rem', borderRadius: '30px' }}>
+                        <div className="card-body">
+                            <h1 className="loginregister">Login Below</h1>
+                            <input
+                                className='field'
+                                type='username'
+                                placeholder='Username'
+                            />
+                            <br />
+                            <input
+                                className='field'
+                                type='password'
+                                placeholder='Password'
+                            />
+                            <br />
+                            <a href='/registration'>
+                                <button className='button btn-secondary'>
+                                    Login
+                                </button>
+                            </a>
+                        </div>
+                    </div>
+                </div>
 
-                 </footer>
+
+
+                <footer className='footer'>
+
+                </footer>
 
             </div>
-    )
-  }
+        )
+    }
 }
 export default Login;
--- src/regstration.js
+++ src/regstration.js
@@ -1,49 +1,59 @@
 import React, { Component } from 'react';
 import './App.css'
-
+import Logo from './images/cauli.jpg';
 class Form extends Component {
     render() {
         return (
             <div className='hello'>
-                <h1 className='loginregister'>Register Below</h1>
-                <input
-                    className='field'
-                    type='name'
-                    placeholder='Name'
-                />
-                <br />
-                <input
-                    className='field'
-                    type='email'
-                    placeholder='Email'
-                />
-                <br />
-                <input
-                    className='field'
-                    type='phone'
-                    placeholder='Phone'
-                />
-                <br />
-                <input
-                    className='field'
-                    type='username'
-                    placeholder='Username'
-                />
-                <br />
-                <input
-                    className='field'
-                    type='password'
-                    placeholder='Password'
-                />
-                <br />
-                <a href='/'>
-                    <button className='button btn-success'>
-                        Register
-                     </button>
-                </a>
-                <br />
+                <div className='images'>
+                    <img className='logo' src={Logo} />
+                </div>
+                <div style={{ backgroundImage: '' }} />
+                <div style={{ paddingLeft: '650px' }}>
+                    <div class="card" style={{ width: '25rem', borderRadius: '30px' }}>
+                        <div class="card-body">
+                            <h1 class="loginregister">Register Below</h1>
+                            <input
+                                className='field'
+                                type='name'
+                                placeholder='Name'
+                            />
+                            <br />
+                            <input
+                                className='field'
+                                type='email'
+                                placeholder='Email'
+                            />
+                            <br />
+                            <input
+                                className='field'
+                                type='phone'
+                                placeholder='Phone'
+                            />
+                            <br />
+                            <input
+                                className='field'
+                                type='username'
+                                placeholder='Username'
+                            />
+                            <br />
+                            <input
+                                className='field'
+                                type='password'
+                                placeholder='Password'
+                            />
+                            <br />
+                            <a href='/'>
+                                <button className='button btn-secondary'>
+                                    Register
+                                </button>
+                            </a>
+
+                        </div>
+                    </div>
+                </div>
                 <footer className='footer'>
-                    
+
                 </footer>
 
             </div>
